5432c53de63a95ba7ff0b0b0aa56f3b04e9faba3
naufalrf4 <naufalrf4@gmail.com>
2024-05-11 03:32:20 +0700

    chore: Update logger file to use dynamic logs directory

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,8 +1,11 @@
 const { createLogger, transports, format } = require('winston');
+const path = require('path');
+
+const logsDirectory = path.join(__dirname, '..', 'logs');
 
 const logger = createLogger({
     transports: [
-        new transports.File({ filename: 'error.log', level: 'error' }),
+        new transports.File({ filename: path.join(logsDirectory, 'error.log'), level: 'error' }),
         new transports.Console({ format: format.combine(format.simple(), format.timestamp()), level: 'info' })
     ],
     exitOnError: false 
